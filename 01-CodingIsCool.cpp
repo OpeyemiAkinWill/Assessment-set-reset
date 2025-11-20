9bd23c71b9451108123c321dc6e4dde81c1ae899
OpeyemiAkinWill <yemifirststep@hotmail.co.uk>
2025-11-20 13:54:40 +0000

    Coding is cool exercise complete.

---
+++ 01-CodingIsCool.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // This is the standard C++ program language, it gives permit user to input and output data such as (cout, cin)
+#include <string> // standard data type instruction for the program to specify type of data type.
+using namespace std; // This line is used instead of using stad::cout inside the main program, otherwise, it must be written before the int main
+
+int main() { // line within the main function
+    cout << "Coding is cool" << endl;  // cout is the instruction for the program to output or display the message or code written - Prints the message and moves to a new line. Endl instruction to enter new line
+    return 0;  // Indicates successful program termination.
+}
